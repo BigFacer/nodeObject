5acaa30f9295f06c662a53b3e587902c35fa2506
BigFacer <grhnavergiveup@163.com>
2020-08-20 14:59:03 +0800

    20200820

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import vuex from 'vuex'
-// import Qs from 'qs'
 import 'element-ui/lib/theme-chalk/index.css'
 import ElementUi from 'element-ui'
 // import { formaDate } from
