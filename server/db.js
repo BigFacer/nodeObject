73113519c17a75bd86233f83f1971a15b097ff75
HeroAhao <184837552@qq.com>
2020-12-13 13:12:02 +0800

    20201213

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 // 数据库配置
 module.exports = {
     mysql: {
-        host: '118.190.98.34',
+        host: '192.144.235.110',
         user: 'root',
-        password: 'Yiqinuli2019',
+        password: 'Yiqinuli2019!',
         port: '3306',
         database: 'user',
         useConnectionPooling: true //使用连接池
